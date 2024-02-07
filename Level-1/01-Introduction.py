40d31e5571ad09e16a47a622efad4555480d651b
Manish Yadav <itp.manis@gmail.com>
2024-02-07 09:47:57 +0545

    Introduction

---
+++ Level-1/01-Introduction.py
@@ -0,0 +1,56 @@
+"""
+⭐️ What is Python?
+- Python is a popular programming language.It is used for: web development (server-side), software development, mathematics, system scripting etc
+"""
+
+"""
+⭐️ What can Python do?
+- Create Web Applications: Django, Pyramid, Bottle, Tornado, Flask, web2py
+- Automate Workflows: Automate system maintenance tasks, such as moving and cleaning up files or running backups.
+- Connect to Databases: It can also be used in database applications such as Oracle Database, MySQL, PostgreSQL, MongoDB, MariaDB, and so on.
+- Handle Big Data and Math: Python is widely used in scientific and numeric computing:
+- Prototyping and Software Development: Python is widely used in prototyping and building software applications.
+"""
+
+"""
+⭐️ Why Python?
+- Python works on different platforms (Windows, Mac, Linux, Raspberry Pi, etc).
+- Python has a simple syntax similar to the English language.
+- Python has syntax that allows developers to write programs with fewer lines than some other programming languages.
+- Python runs on an interpreter system, meaning that code can be executed as soon as it is written. This means that prototyping can be very quick.
+"""
+
+"""
+⭐️ Python Install
+- Python is already installed on Mac and Linux. install it on Windows
+- check in command prompt: python3 --version
+"""
+
+"""
+⭐️ Python File Extensions
+- Python files have the extension .py
+
+"""
+
+"""
+⭐️ Run Python File
+- Run Python in Terminal: python3 copy relevant file path
+"""
+
+
+# This is sinlge line comment
+"""
+- This is multi-line comment
+- This is multi-line comment
+- This is multi-line comment
+"""
+
+
+print("Hello, World!")   # Print Hello, World! to the console
+print("Addition: ", 2+3)  # Output:  Addition:  5
+print("Subtraction: ", 5-3)  # Output:  Subtraction:  2
+print("Multiplication: ", 2*3)  # Output:  Multiplication:  6
+print("Division: ", 6/3)  # Output:  Division:  2.0
+print("Modulus: ", 5 % 3)  # Output:  Modulus:  2
+print("Exponentiation: ", 2**3)  # Output:  Exponentiation:  8
+print("Floor Division: ", 5//3)  # Output:  Floor Division:  1
