de8a66e028b6e7b81eb514ff67d37d05092fffe2
Manish Yadav <itp.manis@gmail.com>
2024-02-07 09:52:54 +0545

    .

---
+++ Level-1/02-Variables.py
@@ -0,0 +1,12 @@
+# ⭐️⭐️⭐️⭐️⭐️ Variables ⭐️⭐️⭐️⭐️⭐️
+"""
+- Variable are nothing but a container which store data in the memory.
+- Variable vanako yuta box ho jasle data lai store garcha. 
+"""
+
+# ⭐️ Variable declaration
+# Syntax: variable_name = value
+name = "Manish"  # We can use single or double quotes
+age = 20              # aru programing languages jasto ; semicolon use garna mildaina
+print(name)         # print() function le hamro data lai console ma display garcha
+print("--"*20)
