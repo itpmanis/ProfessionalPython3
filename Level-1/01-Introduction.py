3f3b13ff4d170f4c538bb9f2122b94284f49ba93
Manish Yadav <itp.manis@gmail.com>
2024-02-19 10:36:35 +0545

    .

---
+++ Level-1/01-Introduction.py
@@ -0,0 +1,8 @@
+"""
+⭐️ What is Python?
+- Python is a popular programming language.It is used for: 
+web development (server-side), 
+software development,
+mathematics, 
+system scripting etc
+"""
