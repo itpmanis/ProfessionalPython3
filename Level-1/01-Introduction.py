3ec5b4d10d42e54e04abe13abb91791894fa51dd
Manish Yadav <itp.manis@gmail.com>
2024-02-19 10:46:55 +0545

    intro completed

--- Level-1/01-Introduction.py
+++ Level-1/01-Introduction.py
@@ -1,8 +1,46 @@
 """
 ⭐️ What is Python?
-- Python is a popular programming language.It is used for: 
-web development (server-side), 
-software development,
-mathematics, 
-system scripting etc
+- Python is a popular programming language.It is used for: web development (server-side), software development, mathematics, system scripting etc
+
+⭐️ What can Python do?
+- Build websites and web applications:       Tools like Django and Flask make web development easy.
+- Automate tasks:       Simplify your work by automating file management, backups, and other repetitive jobs.
+- Work with databases:       Connect to and manage a variety of databases (MySQL, Oracle, etc.).
+- Analyze data and perform calculations:        Ideal for scientific research and handling big datasets.
+- Create software and prototypes:        Quickly build and test new software ideas.
+
+⭐️ Why Python?
+- Python works on different platforms (Windows, Mac, Linux, Raspberry Pi, etc).
+- Python has a simple syntax similar to the English language.
+- Python has syntax that allows developers to write programs with fewer lines than some other programming languages.
+- Python runs on an interpreter system, meaning that code can be executed as soon as it is written. This means that prototyping can be very quick.
+
+
+⭐️ Python Installation
+Mac and Linux: Python typically comes pre-installed on these systems.
+Windows: Download the installer from the official Python website (https://www.python.org/downloads/) and follow the instructions.
+    Verifying Installation
+    - Open your command prompt (Windows) or terminal (Mac/Linux).
+    - Type python3 --version and press Enter.
+    - If installed correctly, you'll see the Python version displayed.
+
+
+⭐️ Python File Extensions
+- Python source code files use the .py extension (e.g., my_script.py).
+
+⭐️ Running Python Files
+- Open your command prompt (Windows) or terminal (Mac/Linux).
+- python3 file_name.py and press Enter.
+
 """
+
+# This is single line comment  
+
+"""
+This is a multi-line comment
+This is a multi-line comment
+This is a multi-line comment
+"""
+
+print("Hello, World!")  # Print is used to display the output on the screen
+print('Namaste Nepal')
