d9bb1640253ba7c6df3420a6e7e9da00dee0c93b
Manish Yadav <itp.manis@gmail.com>
2024-06-11 21:33:09 +0545

    .

--- Level-1/07-Lambda.py
+++ Level-1/07-Lambda.py
@@ -5,20 +5,14 @@
 # lambda function ko syntax:
 # lambda arguments : expression
 
-add=lambda x,y:x+y
-print(add(2,3))
-
-print("--"*20)
-
-sum=lambda x:x+10
+add = lambda x, y: x + y
+print(add(2, 3))
+print("--" * 20)
+sum = lambda x: x + 10
 print(sum(2))
-
-print("--"*20)
-
-x= lambda a,b,c: a+b-c
-print(x(2,3,4))
-
-
+print("--" * 20)
+x = lambda a, b, c: a + b - c
+print(x(2, 3, 4))
 # uses of lambda function
 # - lambda function chai aru function ko vitra use hunxa.
 # - lambda function chai aru function ko return value hunxa.
